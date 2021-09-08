518a462966efc5d4a3884f6209e9c39f91f4d858
Stefania F <steff.20f@gmail.com>
2021-09-08 10:31:57 -0300

    se creó un idex con un canvas y se agregó la funcionalidad de dibujar en el mismo

---
+++ js2.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const can= document.getElementById("draw");
+    can.height= window.innerHeight;
+    can.width= window.innerWidth;
+
+    /** @type {CanvasRenderingContext2D} */
+    const ctx= can.getContext("2d");
+
+
+    //variables
+    let painting= false;
+
+
+    function startPosition() {
+        painting=true;
+        draw(e);
+    }
+
+    function finishedPosition() {
+        painting=false;
+        ctx.beginPath();
+    }
+
+    function draw(e) {
+        if(!painting) return; //no hace nada si no pinta
+        ctx.lineWidth =6;
+        ctx.lineCap = "round";
+        
+        ctx.lineTo(e.clientX, e.clientY);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(e.clientX, e.clientY);
+    }
+
+    //EventListeners
+    can.addEventListener("mousedown", startPosition);
+    can.addEventListener("mouseup", finishedPosition);
+    can.addEventListener("mousemove", draw);
+
+});
