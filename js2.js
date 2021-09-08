695f5d7160cb590118570b6c902b28002a38142b
Stefania F <steff.20f@gmail.com>
2021-09-08 18:01:32 -0300

    se agregaron algunos comentarios sobre el archivo javascript

--- js2.js
+++ js2.js
@@ -1,12 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+    //Buscamos con el id al canvas desde el documento para editarlo desde javascript
     const can= document.getElementById("draw");
     can.height= document.documentElement.clientHeight; 
     can.width= document.documentElement.clientWidth ;
 
+    //Buscamos su contexto y lo guardamos en una variable para poder crear nuestros métodos con los cuales dibujar
     /** @type {CanvasRenderingContext2D} */
     const ctx= can.getContext("2d");
 
+    
+    //Declaramos las variables que necesitamos. En este caso seteamos en false la variable painting que cambia 
+    //de valores booleanos dependiendo si estamos dibujando con nuestro mouse o no. Inicialmente se setea en false ya que
+    //no comenzamos dibujando
+    
     //variables
     let painting= false;
 
@@ -20,6 +27,8 @@ document.addEventListener("DOMContentLoaded", function () {
         ctx.beginPath();
     }
 
+    //Este método es el que permite dibujar, utiliza los métodos que provee la API del contexto. Se utilizan además
+    //las coordenadas del canvas del cliente (browser)
     function draw(e) {
         if(!painting) return; //no hace nada si no pinta
         ctx.lineWidth =6;
@@ -32,8 +41,8 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     //EventListeners
-    can.addEventListener("mousedown", startPosition);
-    can.addEventListener("mouseup", finishedPosition);
-    can.addEventListener("mousemove", draw);
+    can.addEventListener("mousedown", startPosition); //Cuando hacemos click, nos tiene que permitir dibujar, aunque sea un punto
+    can.addEventListener("mouseup", finishedPosition); //Este método permite finalizar el trazado el líneas
+    can.addEventListener("mousemove", draw); //Cuando se mueve el mouse se activa el método para dibujar en el canvas
 
 });
